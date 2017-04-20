b2145436434fc90908182c115eb2bd943f0759c0
PranowTest <pranowojha987@gmail.com>
2017-04-21 01:13:26 +0530

    untracked added

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,19 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def about
+  end
+
+  def event
+  end
+
+  def contact
+  end
+
+  def blogs
+  end
+
+  def carriers
+  end
+end
